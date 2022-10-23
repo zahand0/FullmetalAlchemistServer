a3f8bf6b59f3b283ef9dd80e05d790d4873c9bb7
Sergey <mulichev@gmail.com>
2022-10-23 21:13:16 +0300

    updated repository

--- src/main/kotlin/com/example/repository/HeroRepositoryImpl.kt
+++ src/main/kotlin/com/example/repository/HeroRepositoryImpl.kt
@@ -1,21 +1,20 @@
 package com.example.repository
 
+import com.example.data.heroesList
 import com.example.models.ApiResponse
 import com.example.models.Hero
+import com.example.models.MilitaryRank
 
 class HeroRepositoryImpl: HeroRepository {
-    override val heroes: Map<Int, List<Hero>>
-        get() = TODO("Not yet implemented")
-    override val page1: List<Hero>
-        get() = TODO("Not yet implemented")
-    override val page2: List<Hero>
-        get() = TODO("Not yet implemented")
-    override val page3: List<Hero>
-        get() = TODO("Not yet implemented")
-    override val page4: List<Hero>
-        get() = TODO("Not yet implemented")
-    override val page5: List<Hero>
-        get() = TODO("Not yet implemented")
+    override val heroes: Map<Int, List<Hero>> by lazy {
+        mapOf(
+           1 to heroesList.subList(0, 4),
+           2 to heroesList.subList(4, 8),
+           3 to heroesList.subList(8, 12),
+           4 to heroesList.subList(12, 16),
+           5 to heroesList.subList(16, 20)
+        )
+    }
 
     override suspend fun getAllHeroes(page: Int): ApiResponse {
         TODO("Not yet implemented")

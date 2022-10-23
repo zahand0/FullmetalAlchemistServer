91569377bea270e64ef4af186b036e894843d956
Sergey <mulichev@gmail.com>
2022-10-24 01:03:48 +0300

    updated data

--- src/main/kotlin/com/example/data/HardcodedData.kt
+++ src/main/kotlin/com/example/data/HardcodedData.kt
@@ -18,7 +18,7 @@ val heroesList = listOf(
         rating = 10.00,
         about = "Roy Mustang (ロイ・マスタング Roi Masutangu), also known as the Flame Alchemist (焔の錬金術師 Honō no Renkinjutsushi), is the tritagonist of the Fullmetal Alchemist series. He is a State Alchemist and officer in the Amestrian State Military. A hero of the Ishval Civil War and Edward Elric’s superior officer, Colonel Mustang is a remarkably capable commander who plans to become the next Führer of Amestris.",
         species = Species.Human,
-        militaryRank = MilitaryRank.Colonel,
+        militaryRank = MilitaryRank.Colonel(),
         abilities = listOf(
             "Flame-Based Alchemy"
         ),
@@ -40,7 +40,7 @@ val heroesList = listOf(
         about = "Edward Elric (エドワード・エルリック Edowādo Erurikku) also known as Ed or the Fullmetal Alchemist (鋼の錬金術師, Hagane no Renkinjutsushi), is the titular main protagonist of the Fullmetal Alchemist series.\n" +
         "After losing his right arm and left leg due to a failed Human Transmutation attempt, Ed became the youngest State Alchemist in history by achieving his certification at age twelve. Three years later, Ed has become fully engrossed in his search for the legendary Philosopher's Stone, an item with which he hopes to restore his younger brother Alphonse Elric, whose whole body had been lost in the aftermath of the human transmutation.",
         species = Species.Human,
-        militaryRank = MilitaryRank.Major,
+        militaryRank = MilitaryRank.Major(),
         abilities = listOf(
             "Alchemy",
             "Staff",
@@ -65,7 +65,7 @@ val heroesList = listOf(
         rating = 9.60,
         about = "Ling Yao (リン・ヤオ, Rin Yao), also spelled as Lin Yao, is the twelfth crown prince of the eastern nation of Xing and the liege of the Yao clan. Seeking a way to ensure his path to the throne amidst deadly inter-house competition, Ling has ventured from Xing to Amestris in order to divine the secret of immortality - the Philosopher's Stone.",
         species = Species.Human,
-        militaryRank = MilitaryRank.Civilian,
+        militaryRank = MilitaryRank.Civilian(),
         abilities = listOf(
             "Dao",
             "Grenades",
@@ -90,7 +90,7 @@ val heroesList = listOf(
         rating = 9.40,
         about = "Lieutenant Riza Hawkeye (リザ・ホークアイ, Riza Hōkuai) is an officer in the Amestrian State Military as well as the personal adjutant and bodyguard of Colonel Roy Mustang. A sharpshooter and firearms specialist, Lt. Hawkeye is an invaluable asset to the Colonel both in office and on the battlefield and serves as his closest and most supportive subordinate.",
         species = Species.Human,
-        militaryRank = MilitaryRank.FirstLieutenant,
+        militaryRank = MilitaryRank.FirstLieutenant(),
         abilities = listOf(
             "Sniping",
             "Close range gunfighting",
@@ -115,7 +115,7 @@ val heroesList = listOf(
         rating = 9.20,
         about = "Alphonse Elric (アルフォンス・エルリック Arufonsu Erurikku), also known as Al, is the deuteragonist of the Fullmetal Alchemist series. He is the younger brother of the Fullmetal Alchemist Edward Elric. Al is also a victim of the failed Human Transmutation experiment in which Ed lost his right arm and left leg. Having had his entire physical being taken away from him in the aftermath of the tragedy, Alphonse exists solely as a soul alchemically bound to a large suit of steel armor. Now, Al travels with his brother in search of a legendary Philosopher's Stone, an item with the power to restore their bodies to normal.",
         species = Species.Human,
-        militaryRank = MilitaryRank.Civilian,
+        militaryRank = MilitaryRank.Civilian(),
         abilities = listOf(
             "Alchemy,",
             "Martial Arts,",
@@ -140,7 +140,7 @@ val heroesList = listOf(
         rating = 9.00,
         about = "The Scarred Man (傷の男, Kizu no Otoko), known more casually as \"Scar\" (スカー, Sukā), is a nameless lone serial killer and vigilante who targets State Alchemists for his own brand of lethal justice in accordance with the fundamental tenets of his religion and for the revenge of his slaughtered people and family. An Ishvalan survivor of the Ishvalan Civil War, Scar's moniker comes from the large X-shaped scar on his forehead and sports a conspicuous and intricate alchemical tattoo on his right arm, which originally belonged to his older brother, and which he uses as his murder weapon of choice.",
         species = Species.Human,
-        militaryRank = MilitaryRank.Civilian,
+        militaryRank = MilitaryRank.Civilian(),
         abilities = listOf(
             "Alchemy",
             "Hand-to-hand Combat"
@@ -164,7 +164,7 @@ val heroesList = listOf(
         rating = 8.80,
         about = "Izumi Curtis (イズミ・カーティス, Izumi Kātisu, née Harnet) is a master alchemist and skilled martial artist living in the southern Amestrian town of Dublith, where she runs a butcher shop with her husband Sig. Though usually doesn't take on students, she accepted Edward and Alphonse Elric's plea to taking them on as her apprentices and instructed them in both alchemical theory and martial arts for six months. Eventually, Izumi grew to love them as deeply as she would her own children, becoming a long-term source of guidance and support for the Elrics' future endeavor.",
         species = Species.Human,
-        militaryRank = MilitaryRank.Civilian,
+        militaryRank = MilitaryRank.Civilian(),
         abilities = listOf(
             "Alchemy",
             "Martial Arts"
@@ -187,7 +187,7 @@ val heroesList = listOf(
         rating = 8.60,
         about = "Maes Hughes was an Amestrian State Military officer stationed in the Investigations Office in Central City and Colonel Roy Mustang's best friend, as well as the closest supporter of Mustang's secret bid for the position of Führer. A devoted family man and exemplary soldier, Hughes was generally well-liked by the other characters (unless he was pushing his love of his wife or daughter on them) and his murder served as the major starting point for the story to unfold. After his death, Hughes was promoted two ranks, making him Brigadier General.",
         species = Species.Human,
-        militaryRank = MilitaryRank.LieutenantColonel,
+        militaryRank = MilitaryRank.LieutenantColonel(),
         abilities = listOf(
             "Push knives",
             "Deduction"
@@ -209,7 +209,7 @@ val heroesList = listOf(
         rating = 8.40,
         about = "Greed (グリード, Gurīdo) is the embodiment of some of Father's greed. He is called the \"Ultimate Shield\" because he can rearrange the carbon in his body to make his skin as hard as diamond, thus making him impenetrable by most weapons. As his name implies, he is a very avaricious character, who likes living in luxury, surrounded by powerful minions and beautiful women. He also has a habit of flipping people off in the manga. Despite his generally greedy nature, he is shown to care for his subordinates, as detailed below.",
         species = Species.Homunculus,
-        militaryRank = MilitaryRank.Civilian,
+        militaryRank = MilitaryRank.Civilian(),
         abilities = listOf(
             "Alter the outer layer of his body to shield"
         ),
@@ -232,7 +232,7 @@ val heroesList = listOf(
         rating = 8.20,
         about = "Major Alex Louis Armstrong (アレックス・ルイ・アームストロング, Arekkusu Rui Āmusutorongu), also known as the Strong Arm Alchemist (豪腕の錬金術師, Gōwan no Renkinjutsushi), is a State Alchemist and officer in the Amestrian State Military. The scion of the illustrious Armstrong family, Alex is a remarkably caring commander and friend as well as an invaluably skilled ally to Colonel Roy Mustang and Edward Elric.",
         species = Species.Human,
-        militaryRank = MilitaryRank.Major,
+        militaryRank = MilitaryRank.Major(),
         abilities = listOf(
             "Alchemy",
             "Boxing",
@@ -259,7 +259,7 @@ val heroesList = listOf(
         rating = 8.00,
         about = "King Bradley (キング・ブラッドレイ Kingu Buraddorei?) is one of the primary antagonists in the Fullmetal Alchemist series, being that he is one of the seven Homunculi. Bradley was also the original Führer (大総統 Daisōtō) of Amestris. In the manga series and the 2009 anime reboot, he was known as Wrath (ラース Rāsu), the final homunculus created by Father, but was changed in the 2003 anime to Pride (プライド Puraido?), created by the ancient alchemist Dante. In both series, Bradley is the leader of Amestris and is seen to have almost inhuman swordsmanship. However, in the manga and 2009 anime series, he is an important character being that Amestris was created for the sole purpose of exacting Father's plan to become the ultimate being. As Führer, Bradley is the respected Commander-in-Chief of the State Military, even though his political standpoints are often questioned in certain instances such as the Ishval Civil War, which was only one of many insurrections focused on creating a massive Philosopher's Stone for Father.",
         species = Species.Homunculus,
-        militaryRank = MilitaryRank.FuhrerPresident,
+        militaryRank = MilitaryRank.FuhrerPresident(),
         abilities = listOf(
             "Ultimate Eye",
             "Swordsmanship",
@@ -285,7 +285,7 @@ val heroesList = listOf(
         rating = 7.80,
         about = "Olivier Mira Armstrong (オリヴィエ・ミラ・アームストロング, Orivie Mira Āmusutorongu), also known as Major General Armstrong, is the primary heir to the illustrious Armstrong family, the commanding officer charged with the protection of Amestris' northern border at Fort Briggs and the older sister of Alex Louis Armstrong.",
         species = Species.Human,
-        militaryRank = MilitaryRank.MajorGeneral,
+        militaryRank = MilitaryRank.MajorGeneral(),
         abilities = listOf(
             "Swordsmanship",
             "Leadership",
@@ -309,7 +309,7 @@ val heroesList = listOf(
         rating = 7.60,
         about = "Van Hohenheim is a deceptively ancient and extremely powerful Alchemist as well as the estranged father of Edward and Alphonse Elric. Despite disappearing suddenly during their infancy, Hohenheim returns during the course of the series in order to right the wrongs of his mysterious past.",
         species = Species.Human,
-        militaryRank = MilitaryRank.Civilian,
+        militaryRank = MilitaryRank.Civilian(),
         abilities = listOf(
             "Regeneration",
             "Alchemy"
@@ -332,7 +332,7 @@ val heroesList = listOf(
         about = "Winry Rockbell (ウィンリィ・ロックベル, Uinrī Rokkuberu) is a main supporting character of the Fullmetal Alchemist series.\n" +
         "Winry is a training surgeon and mechanical repair specialist/enthusiast as she is the daughter of Yuriy Rockbell and Sarah Rockbell, two surgeons that worked on the Ishval Civil War and is the grand-daughter of Pinako Rockbell, a well known mechanic and surgeon in her youth. She is a girl from Resembool and the close childhood friend of Edward Elric and Alphonse Elric. Winry also supports Edward as his personal automail engineer and mechanic, making sure his prosthetic right arm and left leg are always in good working order.",
         species = Species.Human,
-        militaryRank = MilitaryRank.Civilian,
+        militaryRank = MilitaryRank.Civilian(),
         abilities = listOf(
             "Skilled Automail specialist and Mechanic",
             "Amateur Surgeon"
@@ -354,7 +354,7 @@ val heroesList = listOf(
         rating = 7.20,
         about = "Lan Fan (ランファン, Ranfan) is a retainer of the Xing Empire's Yao Clan, a personal bodyguard of Prince Ling Yao and the grandchild of Ling's senior bodyguard, Fu. She is fiercely loyal to her prince; insults to his name cause her to fly into a rage (overwhelming her better judgment and dulling her usually sharp combat abilities) and she becomes flustered when unmasked in his presence.",
         species = Species.Human,
-        militaryRank = MilitaryRank.Civilian,
+        militaryRank = MilitaryRank.Civilian(),
         abilities = listOf(
             "Xingese Martial Arts",
             "Detection the chi of humans and Homunculi"
@@ -380,7 +380,7 @@ val heroesList = listOf(
         "Envy, who exists only to make humans suffer, bears more bitter resentment for humans than any other homunculus, and negatively treats with humans in the most personal and heartfelt manner. His hatred for humans has an immense emotional and personal foundation greater than any of his siblings'. Envy can easily be set off by insults due to his large and delicate ego, thus causing him to make mistakes.\n" +
         "Envy is one of the two Homunculi who possesses \"true forms\"—physical incarnations of his traits and personality that take shape when his Stones are almost completely depleted. The other homunculus with a true form is Pride.",
         species = Species.Homunculus,
-        militaryRank = MilitaryRank.Civilian,
+        militaryRank = MilitaryRank.Civilian(),
         abilities = listOf(
             "Shape-shifting",
             "Immortality",
@@ -404,7 +404,7 @@ val heroesList = listOf(
         about = "Mei Chang (also spelled \"May\") is the seventeenth royal princess of Xing, born to the Xingese Emperor and the royal concubine representing the Chang house.\n" +
         "Despite being so young, Mei has been tasked with finding a way to raise the social standing of the poor, downtrodden Chang clan in the eyes of the Emperor and has embarked on a journey to discover and present her father with the secret to immortality. Skilled as she is in the art of Alkahestry, Mei sets out across the desert toward Amestris with her beloved pet panda, Xiao-Mei, so that she may learn about the Philosopher's Stone and, arriving there, seeks out the tutelage of the Fullmetal Alchemist.",
         species = Species.Human,
-        militaryRank = MilitaryRank.Civilian,
+        militaryRank = MilitaryRank.Civilian(),
         abilities = listOf(
             "Martial Arts",
             "sensing the chi of humans and Homunculi",
@@ -429,7 +429,7 @@ val heroesList = listOf(
         rating = 6.60,
         about = "Solf J. Kimblee (ゾルフ・J・キンブリー, Zorufu J Kinburī, also spelled Zolf J. Kimbley), the Crimson Lotus Alchemist (紅蓮の錬金術師, Guren no Renkinjutsushi), sometimes referred to as the Crimson Alchemist, is a recurring antagonist in the Fullmetal Alchemist series. He is a former State Alchemist and special agent working under the direct orders of Führer King Bradley. Admittedly sadistic, Kimblee was imprisoned in Central City for several years after his official involvement in the Ishval Civil War for having infamously turned his unique brand of combustion-based combat alchemy on people who were not on his government-approved kill list. Due to conspiratorial acts at the top of government, Kimblee is officially exonerated of his crimes so as to serve as the Führer's hidden left hand in the secret battles to come.",
         species = Species.Human,
-        militaryRank = MilitaryRank.Major,
+        militaryRank = MilitaryRank.Major(),
         abilities = listOf(
             "Alchemy",
             "Photographic memory",
@@ -452,7 +452,7 @@ val heroesList = listOf(
         rating = 6.40,
         about = "Lust (ラスト, Rasuto) is the embodiment of Father's lust who is mildly flirtatious, but she serves mostly as an object of lust for others, feeding their desires so that she could fulfill her duties to Father. She was usually partnered with Gluttony to whom she appears to have a particular (almost motherly) closeness.",
         species = Species.Homunculus,
-        militaryRank = MilitaryRank.Civilian,
+        militaryRank = MilitaryRank.Civilian(),
         abilities = listOf(
             "Claws creation",
             "Immense beauty/seduction"
@@ -475,7 +475,7 @@ val heroesList = listOf(
         rating = 6.20,
         about = "Gluttony is the embodiment of some of Father's gluttony. Often, he will be found eating a random person or creature's remains unless ordered to eat someone that poses a threat, or to remove evidence of the Homunculi's presence. He is typically in the company of Lust.",
         species = Species.Homunculus,
-        militaryRank = MilitaryRank.Civilian,
+        militaryRank = MilitaryRank.Civilian(),
         abilities = listOf(
             "Able to consume any known material",
             "Advanced sense of smell"

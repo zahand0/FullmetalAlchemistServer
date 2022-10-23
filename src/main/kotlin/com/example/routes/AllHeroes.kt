d0963df9ffe7f3800323dd7c3e3f8745e1e2e810
Sergey <mulichev@gmail.com>
2022-10-23 22:03:28 +0300

    implemented successfull respond

--- src/main/kotlin/com/example/routes/AllHeroes.kt
+++ src/main/kotlin/com/example/routes/AllHeroes.kt
@@ -1,23 +1,31 @@
 package com.example.routes
 
 import com.example.models.ApiResponse
+import com.example.repository.HeroRepository
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import org.koin.ktor.ext.inject
 import java.lang.IllegalArgumentException
 import java.lang.NumberFormatException
 
 fun Route.getAllHeroes() {
+
+    val heroRepository: HeroRepository by inject()
+
     get("/fma/heroes") {
         try {
-            val page = call.request.queryParameters["page"]?.toInt() ?: 1
-            require(page in 1..5)
+            val page = call.request.queryParameters["page"]?.toInt() ?: 0
+
+            val maxPageNumber = heroRepository.heroes.size - 1
+            require(page in 0..maxPageNumber)
+
+            val apiResponse = heroRepository.getAllHeroes(page = page)
+
             call.respond(
-                message = ApiResponse(
-                    success = true,
-                    message = page.toString()
-                )
+                message = apiResponse,
+                status = HttpStatusCode.OK
             )
         } catch (e: NumberFormatException) {
             call.respond(

a05763b3ffae19855cd7a5aee7fbac75f65c3d30
Sergey <mulichev@gmail.com>
2022-10-24 01:27:11 +0300

    catching 404 status

--- src/main/kotlin/com/example/plugins/StatusPages.kt
+++ src/main/kotlin/com/example/plugins/StatusPages.kt
@@ -1,8 +1,18 @@
 package com.example.plugins
 
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.response.*
+import javax.naming.AuthenticationException
 
 fun Application.configureStatusPages() {
-    install(StatusPages)
+    install(StatusPages) {
+        status(HttpStatusCode.NotFound) { call, statusCode ->
+            call.respond(
+                message = "Page not found.",
+                status = statusCode
+            )
+        }
+    }
 }

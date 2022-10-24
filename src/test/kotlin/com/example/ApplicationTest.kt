6bd05e0ca40ae5cdbc68fd99e35ec0ba27ab1b0a
Sergey <mulichev@gmail.com>
2022-10-24 03:04:00 +0300

    added heros routing testing

--- src/test/kotlin/com/example/ApplicationTest.kt
+++ src/test/kotlin/com/example/ApplicationTest.kt
@@ -1,29 +1,115 @@
 package com.example
 
-import io.ktor.server.routing.*
-import io.ktor.http.*
-import io.ktor.serialization.kotlinx.json.*
-import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.plugins.callloging.*
-import org.slf4j.event.*
-import io.ktor.server.request.*
-import io.ktor.server.application.*
-import io.ktor.server.response.*
+import com.example.di.koinModule
+import com.example.models.ApiResponse
+import com.example.models.Hero
+import com.example.repository.HeroRepository
+import com.example.repository.HeroRepositoryImpl
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
-import kotlin.test.*
+import io.ktor.http.*
 import io.ktor.server.testing.*
-import com.example.plugins.*
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.dsl.module
+import org.koin.java.KoinJavaComponent.inject
+import org.koin.test.KoinTest
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class ApplicationTest {
+
+    private val heroRepository: HeroRepository by inject(HeroRepository::class.java)
+
+
     @Test
-    fun testRoot() = testApplication {
-        application {
-            configureRouting()
-        }
+    fun `access root endpoint, assert correct information`() = testApplication {
         client.get("/").apply {
-            assertEquals(HttpStatusCode.OK, status)
-            assertEquals("Welcome to Fullmetal Alchemist API", bodyAsText())
+            assertEquals(
+                expected = HttpStatusCode.OK,
+                actual = status
+            )
+            assertEquals(
+                expected = "Welcome to Fullmetal Alchemist API",
+                actual = bodyAsText()
+            )
         }
     }
+
+    @Test
+    fun `access all heroes endpoint, assert correct information`() = testApplication {
+        environment {
+            developmentMode = false
+        }
+
+
+        client.get("/fma/heroes").apply {
+            assertEquals(
+                expected = HttpStatusCode.OK,
+                actual = status
+            )
+            val expected = ApiResponse(
+                success = true,
+                message = "ok",
+                prevPage = null,
+                nextPage = 1,
+                heroes = heroRepository.heroes[0]
+            )
+            val actual = Json.decodeFromString<ApiResponse>(bodyAsText())
+
+            assertEquals(
+                expected = expected,
+                actual = actual
+            )
+        }
+    }
+
+    @Test
+    fun `access all heroes endpoint, query all pages, assert correct information`() = testApplication {
+        environment {
+            developmentMode = false
+        }
+        client.get("").apply {
+            val pages = heroRepository.heroes.indices
+            val heroes = heroRepository.heroes
+
+            pages.forEach { page ->
+                println("CURRENT PAGE: $page")
+                client.get("/fma/heroes?page=$page").apply {
+                    assertEquals(
+                        expected = HttpStatusCode.OK,
+                        actual = status
+                    )
+                    val expected = ApiResponse(
+                        success = true,
+                        message = "ok",
+                        prevPage = calculatePrevPage(page),
+                        nextPage = calculateNextPage(page, heroes.size),
+                        heroes = heroes[page]
+                    )
+                    val actual = Json.decodeFromString<ApiResponse>(bodyAsText())
+
+                    assertEquals(
+                        expected = expected,
+                        actual = actual
+                    )
+                }
+            }
+        }
+
+
+    }
+
+    private fun calculatePrevPage(page: Int): Int? {
+        return if (page > 0) page - 1 else null
+    }
+
+    private fun calculateNextPage(page: Int, maxPages: Int): Int? {
+        return if (page < maxPages - 1) page + 1 else null
+    }
+
 }
